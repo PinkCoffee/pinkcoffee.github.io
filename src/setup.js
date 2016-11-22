b924b4c59d5e3a0eaf7616db94f18bcc54f451cf
marfur9 <marfur9@hotmail.com>
2016-11-22 19:28:00 +0100

    idunno

--- src/setup.js
+++ src/setup.js
@@ -656,6 +656,36 @@ function setupGrass(terrain){
 
 }
 
+function setupSatelite(objectMaterialLoader) {
+    "use strict";
+
+
+    objectMaterialLoader.load(
+        'models/satelite/UHFSatcom.obj',
+        'models/satelite/UHFSatcom.mtl',
+        function (loadedObject) {
+            "use strict";
+            // Custom function to handle what's supposed to happen once we've loaded the model
+
+            var bbox = new THREE.Box3().setFromObject(loadedObject);
+            console.log(bbox);
+            var object = loadedObject.clone();
+
+            // We should know where the bottom of our object is
+            object.position.y = 4000;
+            object.position.x = 10000;
+            object.position.z = 0;
+
+
+            object.scale.set(2, 2, 2);
+
+            object.name = "satelite";
+
+            scene.add(object);
+    }, onProgress, onError);
+}
+
+
 function generateGaussPositionAndCorrectHeight(terrain, center, radius) {
     "use strict";
     var pos = randomGaussPositionMaker(center, radius);
